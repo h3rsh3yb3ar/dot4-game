3470c1599756ac44ddd5b4f415540936253899f0
Unknown <superdotaplaya@gmail.com>
2018-06-20 21:50:27 -0400

    fixed announcers and added correct victory messages

--- scripts/vscripts/events.lua
+++ scripts/vscripts/events.lua
@@ -398,11 +398,13 @@ function Trialsofretribution:OnCustomTowerKilled( keys )
     if(tower_name == "radiantbot1" or tower_name == "radiantbot2" or tower_name == "radiantbot3" or tower_name == "radiantmid1" or tower_name == "radiantmid2" or tower_name == "radiantmid3" or tower_name == "radianttop1" or tower_name == "radianttop2" or tower_name == "radianttop3" or tower_name == "radianttop4" or tower_name == "radiantbot4") then
 EmitAnnouncerSound("announcer_ann_custom_tower_fall_10") end
     if(tower_name == "direbot1" or tower_name == "direbot2" or tower_name == "direbot3" or tower_name == "diremid1" or tower_name == "diremid2" or tower_name == "diremid3" or tower_name == "diretop1" or tower_name == "diretop2" or tower_name == "diretop3" or tower_name == "diretop4" or tower_name == "direbot4") then
-EmitAnnouncerSound("announcer_ann_custom_tower_fall_13")end
+EmitAnnouncerSound("announcer_ann_custom_tower_fall_06")end
 	  if(tower_name == "fallbot1" or tower_name == "fallbot2" or tower_name == "fallbot3" or tower_name == "fallmid1" or tower_name == "fallmid2" or tower_name == "fallmid3" or tower_name == "falltop1" or tower_name == "falltop2" or tower_name == "falltop3" or tower_name == "falltop4" or tower_name == "fallbot4") then
 EmitAnnouncerSound("announcer_ann_custom_tower_fall_02")end
-	  if(tower_name == "altiarbot1" or tower_name == "altiarbot2" or tower_name == "altiarbot3" or tower_name == "altiarmid1" or tower_name == "altiarmid2" or tower_name == "altiarmid3" or tower_name == "altiartop1" or tower_name == "altiartop2" or tower_name == "altiartop3" or tower_name == "altiartop4" or tower_name == "altiarbot4") then
-EmitAnnouncerSound("announcer_ann_custom_tower_fall_06")end
+	      if(tower_name == "snowbot1" or tower_name == "snowbot2" or tower_name == "snowbot3" or tower_name == "snowmid1" or tower_name == "snowmid2" or tower_name == "snowmid3" or tower_name == "snowtop1" or tower_name == "snowtop2" or tower_name == "snowtop3" or tower_name == "snowtop4" or tower_name == "snowbot4") then
+ 
+EmitAnnouncerSound("announcer_ann_custom_tower_fall_13")end
+ 
  
     local tower_num = tonumber(tower_name:sub(string.len(tower_name),string.len(tower_name)))
    
@@ -425,6 +427,22 @@ EmitAnnouncerSound("announcer_ann_custom_tower_fall_06")end
 	
 	--Check to see if the tower was denied
 	if killer:GetTeam() == killedUnit:GetTeam() then
+     if(tower_name == "radiantbot1" or tower_name == "radiantbot2" or tower_name == "radiantbot3" or tower_name == "radiantmid1" or tower_name == "radiantmid2" or tower_name == "radiantmid3" or tower_name == "radianttop1" or tower_name == "radianttop2" or tower_name == "radianttop3" or tower_name == "radianttop4" or tower_name == "radiantbot4") then
+ 
+EmitAnnouncerSound("announcer_ann_custom_tower_deny_10") end
+ 
+    if(tower_name == "direbot1" or tower_name == "direbot2" or tower_name == "direbot3" or tower_name == "diremid1" or tower_name == "diremid2" or tower_name == "diremid3" or tower_name == "diretop1" or tower_name == "diretop2" or tower_name == "diretop3" or tower_name == "diretop4" or tower_name == "direbot4") then
+ 
+EmitAnnouncerSound("announcer_ann_custom_tower_deny_06")end
+ 
+    if(tower_name == "fallbot1" or tower_name == "fallbot2" or tower_name == "fallbot3" or tower_name == "fallmid1" or tower_name == "fallmid2" or tower_name == "fallmid3" or tower_name == "falltop1" or tower_name == "falltop2" or tower_name == "falltop3" or tower_name == "falltop4" or tower_name == "fallbot4") then
+ 
+EmitAnnouncerSound("announcer_ann_custom_tower_deny_02")end
+ 
+    if(tower_name == "snowbot1" or tower_name == "snowbot2" or tower_name == "snowbot3" or tower_name == "snowmid1" or tower_name == "snowmid2" or tower_name == "snowmid3" or tower_name == "snowtop1" or tower_name == "snowtop2" or tower_name == "snowtop3" or tower_name == "snowtop4" or tower_name == "snowbot4") then
+ 
+EmitAnnouncerSound("announcer_ann_custom_tower_deny_13")end
+ 
 		--Loop through each of the four teams
 		for i=6,9,1 do
 			--Confirm that team getting gold is not the tower owner's team
@@ -454,10 +472,14 @@ function Trialsofretribution:OnFortKilled( keys )
   local deadAncient = EntIndexToHScript(keys.entindex_killed )
   local killedTeamNumber = deadAncient:GetTeamNumber()
   _G.fortsalive = _G.fortsalive - 1
-  if(killedTeamNumber == 6) then EmitAnnouncerSound("announcer_ann_custom_defeated_06")end
-  if(killedTeamNumber == 7) then EmitAnnouncerSound("announcer_ann_custom_defeated_02")end
-  if(killedTeamNumber == 8) then EmitAnnouncerSound("announcer_ann_custom_defeated_10")end
-  if(killedTeamNumber == 9) then EmitAnnouncerSound("announcer_ann_custom_defeated_13")end
+if(killedTeamNumber == 6) then EmitAnnouncerSound("announcer_ann_custom_defeated_13")end -- red
+ 
+  if(killedTeamNumber == 7) then EmitAnnouncerSound("announcer_ann_custom_defeated_02")end -- yellow
+ 
+  if(killedTeamNumber == 8) then EmitAnnouncerSound("announcer_ann_custom_defeated_10")end -- Green
+ 
+  if(killedTeamNumber == 9) then EmitAnnouncerSound("announcer_ann_custom_defeated_06")end -- Blue
+ 
 local fow = Entities:FindByName( nil, "mid"):GetAbsOrigin()
     AddFOWViewer(killedTeamNumber, fow, 9999999, 999999999, false)
     local fow = Entities:FindByName( nil, "radiant"):GetAbsOrigin()
@@ -473,7 +495,7 @@ local fow = Entities:FindByName( nil, "mid"):GetAbsOrigin()
     local fow = Entities:FindByName( nil, "redblue"):GetAbsOrigin()
     AddFOWViewer(killedTeamNumber, fow, 9999999, 999999999, false)
     local fow = Entities:FindByName( nil, "bluegrey"):GetAbsOrigin()
-    AddFOWViewer(killedTeamNumber, fow, 9999999, 999999999, false)
+    AddFO WViewer(killedTeamNumber, fow, 9999999, 999999999, false)
         local fow = Entities:FindByName( nil, "greygreen"):GetAbsOrigin()
     AddFOWViewer(killedTeamNumber, fow, 9999999, 999999999, false)
     for playerId = 0,19 do
@@ -500,6 +522,22 @@ local heroes = HeroList:GetAllHeroes()
 for number,entity in pairs(heroes) do
 local winner = entity:GetTeam()
 if winner ~= _G.deadteam then
+  if winner == 6 then
+  GameRules:SetCustomVictoryMessage( "Red Team Victory!" )
+  EmitAnnouncerSound ("announcer_ann_custom_victory_13")
+end
+  if winner == 7 then
+  GameRules:SetCustomVictoryMessage( "Yellow Team Victory!" )
+  EmitAnnouncerSound ("announcer_ann_custom_victory_02"
+end
+  if winner == 8 then
+  GameRules:SetCustomVictoryMessage( "Green Team Victory!" )
+  EmitAnnouncerSound ("announcer_ann_custom_victory_10")
+end
+  if winner == 9 then
+  GameRules:SetCustomVictoryMessage( "Blue Team Victory!" )
+  EmitAnnouncerSound ("announcer_ann_custom_victory_06")
+end
 GameRules:SetGameWinner(winner)
 GameRules:SetSafeToLeave(true)
     end
