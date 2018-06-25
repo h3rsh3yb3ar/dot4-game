a69d3813105725aba9e46475f8383a688c908da4
Unknown <superdotaplaya@gmail.com>
2018-06-25 16:13:04 -0400

    enabled willow and pango increased rune minimap icon size and camera distance is further back now

--- scripts/vscripts/settings.lua
+++ scripts/vscripts/settings.lua
@@ -15,11 +15,11 @@ GOLD_PER_TICK = 1                    -- How much gold should players get per tic
 GOLD_TICK_TIME = .6                      -- How long should we wait in seconds between gold ticks?
 
 RECOMMENDED_BUILDS_DISABLED = false     -- Should we disable the recommened builds for heroes
-CAMERA_DISTANCE_OVERRIDE = -1           -- How far out should we allow the camera to go?  Use -1 for the default (1134) while still allowing for panorama camera distance changes
+CAMERA_DISTANCE_OVERRIDE = 1350          -- How far out should we allow the camera to go?  Use -1 for the default (1134) while still allowing for panorama camera distance changes
 
 MINIMAP_ICON_SIZE = .75                   -- What icon size should we use for our heroes?
 MINIMAP_CREEP_ICON_SIZE = .75             -- What icon size should we use for creeps?
-MINIMAP_RUNE_ICON_SIZE = .75              -- What icon size should we use for runes?
+MINIMAP_RUNE_ICON_SIZE = 1              -- What icon size should we use for runes?
 
 RUNE_SPAWN_TIME = 120                   -- How long in seconds should we wait between rune spawns?
 CUSTOM_BUYBACK_COST_ENABLED = false      -- Should we use a custom buyback cost setting?
